c293ff3c38af6d4b61102ea8da409f95b85fbed3
BrianOjeda <brian_ezequiel_ojeda@hotmail.com>
2016-09-24 02:36:45 -0300

    terminado, faltan detalles

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,19 +1,170 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngCordova'])
 
 .controller('DashCtrl', function($scope,$cordovaVibration,$cordovaNativeAudio,$cordovaFile) {
-
-
-
-
 
 $scope.vibrar=vibrar;
 
   $scope.prendersonido=prendersonido;
     $scope.apagarsonido=apagarsonido;
-   
-function prendersonido()
+    $scope.terminar=terminar;
+
+crear();
+
+
+
+
+function terminar()
 {
-  alert("sonando con ritmo y sustancia");
+
+   // READ
+    $cordovaFile.readAsText($scope.inputs.readFile)
+      .then(function (success) {
+        // success
+        $("#mostrartxt").html("<h1>"+success+"</h1>");
+      }, function (error) {
+        // error
+        $("#mostrartxt").html("<h1>"+error+"</h1>");
+      });
+
+}
+
+
+
+function eliminar()
+{
+   $cordovaFile.removeFile("archivos/file.txt")
+      .then(function (success) {
+        // success
+      }, function (error) {
+        // error
+      });
+}
+
+
+  function crear()
+  {
+    $cordovaFile.createFile("archivos/file.txt", true)
+      .then(function (success) {
+        // success
+      }, function (error) {
+        // error
+      });
+  }
+ function escribir(escribo)
+ {
+    
+       switch(escribo) {
+      case 'skiner':
+           $cordovaFile.writeExistingFile("archivos/file.txt", "skiner")
+                .then(function (success) {
+                  // success
+                }, function (error) {
+                  // error
+                });
+          break;
+    case 'bart':
+         $cordovaFile.writeExistingFile("archivos/file.txt", "bart")
+                .then(function (success) {
+                  // success
+                }, function (error) {
+                  // error
+                });
+        break;
+       case 'jefe':
+         $cordovaFile.writeExistingFile("archivos/file.txt", "jefe")
+                .then(function (success) {
+                  // success
+                }, function (error) {
+                  // error
+                });
+        break;
+     case 'lisa':
+         $cordovaFile.writeExistingFile("archivos/file.txt", "lisa")
+                .then(function (success) {
+                  // success
+                }, function (error) {
+                  // error
+                });
+        break;
+     case 'marge':
+        $cordovaFile.writeExistingFile("archivos/file.txt", "marge")
+                .then(function (success) {
+                  // success
+                }, function (error) {
+                  // error
+                });
+        break;
+    default:
+        
+      }
+ }   
+   
+function prendersonido(prendo)
+{
+
+  //alert("sonando con ritmo y sustancia");
+   
+    apagarsonido();
+    switch(prendo) {
+      case 'skiner':
+        //alert("anda la osa");
+           $cordovaNativeAudio
+            .preloadSimple('click', 'audio/skiner.mp3')
+            .then(function (msg) {
+              $cordovaNativeAudio.play('click');
+              $cordovaNativeAudio.loop('music');
+            }, function (error) {
+              alert(error);
+            });
+          break;
+    case 'bart':
+         $cordovaNativeAudio
+          .preloadSimple('click', 'audio/bart.mp3')
+          .then(function (msg) {
+            $cordovaNativeAudio.play('click');
+            $cordovaNativeAudio.loop('music');
+          }, function (error) {
+            alert(error);
+          });
+        break;
+       case 'jefe':
+         $cordovaNativeAudio
+          .preloadSimple('click', 'audio/jefe.mp3')
+          .then(function (msg) {
+            $cordovaNativeAudio.play('click');
+            $cordovaNativeAudio.loop('music');
+          }, function (error) {
+            alert(error);
+          });
+        break;
+     case 'lisa':
+         $cordovaNativeAudio
+          .preloadSimple('click', 'audio/lisa.mp3')
+          .then(function (msg) {
+            $cordovaNativeAudio.play('click');
+            $cordovaNativeAudio.loop('music');
+          }, function (error) {
+            alert(error);
+          });
+        break;
+     case 'marge':
+         $cordovaNativeAudio
+          .preloadSimple('click', 'audio/marge.mp3')
+          .then(function (msg) {
+            $cordovaNativeAudio.play('click');
+            $cordovaNativeAudio.loop('music');
+          }, function (error) {
+            alert(error);
+          });
+        break;
+    default:
+        
+      }
+     
+       escribir(prendo);
+ }
+
+  
  /* $cordovaNativeAudio
     .preloadComplex('music', 'audio/music.mp3', 1, 1)
     .then(function (msg) {
@@ -22,19 +173,11 @@ function prendersonido()
       console.error(error);
     });
 */
- $cordovaNativeAudio
-    .preloadSimple('click', 'audio/click.mp3')
-    .then(function (msg) {
-      $cordovaNativeAudio.play('click');
-      $cordovaNativeAudio.loop('music');
-    }, function (error) {
-      alert(error);
-    });
-  }
+ 
+  
 
 function apagarsonido()
 {
-  alert("apagar sonido");
   // stop 'music' loop and unload
    $cordovaNativeAudio.stop('music');
    $cordovaNativeAudio.unload('click');
